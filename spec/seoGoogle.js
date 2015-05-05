b2af42177841142d4ee9ea6662b768c0350e2bea
Hash543 <alishaqoo@gmail.com>
2015-05-06 01:34:02 +0800

    seo google done

--- spec/seoGoogle.js
+++ spec/seoGoogle.js
@@ -1,28 +1,115 @@
-var b = browser.driver;
+//此處帶入使用者參數
+/* 參數(存放在git ignore 的config)
+1.入口 google or google
+2.須要強化的關鍵字
+3.delay 時間
+*/
 var _ = require('lodash');
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-    b.get('http://tw.yahoo.com');
+var fs = require('fs');
+var chromeLocation = require('chrome-location');
+var seoConfig = require('../config/seoConfig');
+var seo = seoConfig.seo;
+var scConfig = seoConfig.scConfig;
+var gconfig = JSON.parse(fs.readFileSync(scConfig.google));
+if(_.isUndefined(gconfig.keyword)){
+    console.log("gconfig is undefined!!!");
+    throw "gconfig is undefined!!!";
+}
+var i;
+var permote = false;
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
 
-    b.findElement(By.name('p')).sendKeys('write a protractor test');
-    b.findElement(by.id('UHSearchWeb')).click().then(function(){
-    	b.findElements(by.css('.res')).then(function(ttt){
-    		ttt.map(function(v,k){
-    			v.getText().then(function(text){
-    				console.log(text);
-    			});
-    			console.log(v.getText());
-    		});
-    	});
-    	b.getTitle().then(function(title){
-    		console.log("title");
-    		console.log(title);
-    	});
+var b = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+var changePage = function(){
+    var pageChanged = false;
+    console.log("   --- should change page!");
+    b.findElements(By.css('a.pn')).then(function(next){
+        if(next.length > 0){
+            next[0].click();
+        }else{
+            b.findElements(By.css('#pg-next')).then(function(pgnext){
+                if(pgnext.length > 0){
+                    pgnext[0].click();
+                    pageChanged = true;
+                    searchResultFilter();
+                }
+            });
+        }
+    }).then(function(){
+        if(pageChanged === false){
+            searchResultFilter();
+        }
     });
-
-	b.sleep(5000);
-    //var todoList = element.all(by.repeater('todo in todos'));
-    //expect(todoList.count()).toEqual(3);
-    expect(1).toEqual(1);
-  });
-});
+}
+var matchingPromise = function(link){
+    link.getAttribute('href').then(function(attr){
+        console.log("  --href:" + attr);
+        if(attr.match(gconfig.url) ){
+            link.click();
+            console.log("return true;");
+            return true;
+        }else{
+            return false;
+        }
+    });
+}
+var searchResultFilter = function(){
+    var linkMatch = false;
+    var matchResult = null;
+    b.sleep(_.random(2000, 8000));
+    b.findElements(By.css('.srg .g .r a')).then(function(t){
+        t.map(function(v , k){
+            b.getTitle().then(function(title){
+                console.log(title);
+                console.log(linkMatch);
+                if(linkMatch != true){
+                    b.executeScript('window.scrollTo(0,'+ (200*(k+1)) +');').then();
+                    b.sleep(_.random(500 , 1000));
+                    //過濾蒐尋結果的連結網址
+                    v.getAttribute('href').then(function(attr){
+                        console.log("  --href:" + attr);
+                        if(attr.match(gconfig.url) ){
+                            matchResult = v;
+                        }
+                    });
+                }
+            });
+        });
+    }).then(function(){
+        //進行換頁的動作
+        //需要注意 google 頁面有兩種版型
+        console.log("進行換頁的動作");
+        console.log(linkMatch);
+        if(matchResult === null){
+            changePage();
+        }else{
+            matchResult.click();
+        }
+    });
+    /*b.getTitle().then(function(title){
+        console.log("title");
+        console.log(title);
+    });*/
+}
+var run = function(){
+    //console.log(fs.readFileSync(chromeLocation + "\\..\\..\\user data\\default\\cookies"));
+    //fs.openSync(chromeLocation + "\\..\\..\\user data\\default\\cookies");
+    //fs.openSync(process.env.APPDATA + "\\Mozilla\\Firefox\\Profiles\\p5577gez.default");
+    //console.log(process.env.APPDATA);
+    console.log("patten:");
+    console.log("  --keyword:" + gconfig.keyword);
+    console.log("  --url:" + gconfig.url);
+    b.get('http://www.google.com/ncr');
+    b.sleep(_.random(2000, 8000));
+    b.findElement(By.name('q')).sendKeys(gconfig.keyword);
+    b.findElement(By.name('btnG')).click().then(function(){
+        searchResultFilter();
+    });
+    b.sleep(5000);
+    b.quit();
+}
+run();
